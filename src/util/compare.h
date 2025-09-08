3d4f0ec26d96025c2cf74299f5ebb48ac7988657
liuyulvv <liuyulvv@outlook.com>
2025-09-08 15:31:54 +0000

    feat: floating-point number compare

---
+++ src/util/compare.h
@@ -0,0 +1,79 @@
+#pragma once
+
+/**
+ * @file compare.h
+ * @author liuyulvv (liuyulvv@outlook.com)
+ * @date 2025-09-03
+ */
+
+#include <cmath>
+
+namespace euclid::util
+{
+
+/**
+ * @brief Compare two floating-point numbers for equality within a specified tolerance.
+ *
+ * @param a First floating-point number.
+ * @param b Second floating-point number.
+ * @param eps Tolerance for comparison (default is 1e-6).
+ * @return true if the numbers are considered equal within the tolerance, false otherwise.
+ */
+bool equal(double a, double b, double eps = 1e-6)
+{
+    return std::abs(a - b) < eps;
+}
+
+/**
+ * @brief Compare two floating-point numbers for less-than within a specified tolerance.
+ *
+ * @param a First floating-point number.
+ * @param b Second floating-point number.
+ * @param eps Tolerance for comparison (default is 1e-6).
+ * @return true if a is less than b considering the tolerance, false otherwise.
+ */
+bool less(double a, double b, double eps = 1e-6)
+{
+    return a < b - eps;
+}
+
+/**
+ * @brief Compare two floating-point numbers for greater-than within a specified tolerance.
+ *
+ * @param a First floating-point number.
+ * @param b Second floating-point number.
+ * @param eps Tolerance for comparison (default is 1e-6).
+ * @return true if a is greater than b considering the tolerance, false otherwise.
+ */
+bool greater(double a, double b, double eps = 1e-6)
+{
+    return a > b + eps;
+}
+
+/**
+ * @brief Compare two floating-point numbers for less-than-or-equal within a specified tolerance.
+ *
+ * @param a First floating-point number.
+ * @param b Second floating-point number.
+ * @param eps Tolerance for comparison (default is 1e-6).
+ * @return true if a is less than or equal to b considering the tolerance, false otherwise
+ */
+bool less_equal(double a, double b, double eps = 1e-6)
+{
+    return less(a, b, eps) || equal(a, b, eps);
+}
+
+/**
+ * @brief Compare two floating-point numbers for greater-than-or-equal within a specified tolerance.
+ *
+ * @param a First floating-point number.
+ * @param b Second floating-point number.
+ * @param eps Tolerance for comparison (default is 1e-6).
+ * @return true if a is greater than or equal to b considering the tolerance, false otherwise.
+ */
+bool greater_equal(double a, double b, double eps = 1e-6)
+{
+    return greater(a, b, eps) || equal(a, b, eps);
+}
+
+} // namespace euclid::util
